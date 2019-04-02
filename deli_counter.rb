b7b1d44a41bbd82b2f4da5aa190b9557ac7685fe
remarkable-markup-1310 <wonsae.kim@gmail.com>
2019-04-02 14:01:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,32 +1,35 @@
 # Write your code here.
-def line(array)
-  message = ""
-  if array.empty?
-    message += "The line is currently empty."
+def line(deli)
+  msg = ""
+  if deli.empty?
+    msg += "The line is currently empty."
   else
-    message += "The line is currently: "
+    msg += "The line is currently: "
+    
+    deli.each_with_index do |name, index|
+      if index == deli.length - 1
+        msg += "#{index + 1}. #{name}"
+      else
+        msg += "#{index + 1}. #{name} "
+      end
+    end
   end
   
-  array.each_with_index {|e, i|
-    if i == array.length - 1
-      message += "#{i+1}. #{e}" 
-    else
-      message += "#{i+1}. #{e} "
-    end
-  }
-  puts message
+  puts msg
 end
 
-def take_a_number(array, name)
-  array << name
-  count = array.length
-  puts "Welcome, #{name}. You are number #{count} in line."
+def take_a_number(deli, name)
+  deli.push(name)
+  num = deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{num} in line."
 end
 
-def now_serving(array)
-  if array.empty?
-    puts "There is nobody waiting to be served!"
+def now_serving(deli)
+  serving = deli.shift
+  if deli.empty? 
+    puts "There is nobody waiting to be served!" 
   else
-    puts "Currently serving #{array.shift}."
+    puts "Currently serving #{serving}."
   end
 end
+
